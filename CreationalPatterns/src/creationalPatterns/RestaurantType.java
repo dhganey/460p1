8454153992c169557b3ec7c8efd3ef41ed3797f5
David Ganey <dhganey@gmail.com>
2014-10-23 10:32:02 -0700

    Revise reflection instantiation to use enum toString instead of array

--- CreationalPatterns/src/creationalPatterns/RestaurantType.java
+++ CreationalPatterns/src/creationalPatterns/RestaurantType.java
@@ -3,4 +3,28 @@ package creationalPatterns;
 public enum RestaurantType
 {
 	DINER, EVENING_ONLY, ALL_DAY;
+	
+	//http://stackoverflow.com/questions/9662170/java-override-valueof-and-tostring-in-enum	
+	
+	@Override
+	public String toString()
+	{
+		return this.getValue();
+	}
+	
+	private String getValue()
+	{
+		if (this.equals(ALL_DAY))
+		{
+			return "AllDay";
+		}
+		else if (this.equals(EVENING_ONLY))
+		{
+			return "EveningOnly";
+		}
+		else
+		{
+			return "Diner";
+		}
+	}
 }
